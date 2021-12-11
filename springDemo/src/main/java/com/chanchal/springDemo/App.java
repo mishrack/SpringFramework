b76b9c61d22b1b283e00d91b8b797bd4289a544d
ck_mishra <35187244+chanchal4u@users.noreply.github.com>
2021-12-12 00:13:47 +0530

    Spring Boot Introduction.. Chapter 1

---
+++ springDemo/src/main/java/com/chanchal/springDemo/App.java
@@ -0,0 +1,13 @@
+package com.chanchal.springDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
