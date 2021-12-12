1505f1a114f8c20e88a4f353d5ebe830eb589acb
ck_mishra <35187244+chanchal4u@users.noreply.github.com>
2021-12-12 15:01:36 +0530

    Chapter 3 : Spring MVC using Spring Boot

---
+++ springmvc/src/main/resources/com/chanchal/AddController.java
@@ -0,0 +1,15 @@
+package com.chanchal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController 
+{
+	@RequestMapping("/add")
+	public String add()
+	{
+		return "display.jsp" ;
+//		System.out.println("Hello!!.. I am here learning Spring MVC");
+	}
+}
